f74d80ef12bb0d3949d1907c8c6f95e87047f447
Nick Haynes <nickhaynes@Nicks-Mac-mini.local>
2021-07-24 21:13:24 -0500

    First commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+    @foreach ($posts as $post)
+        <article class="{{ $loop->even ? 'foobar' : '' }}">
+
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
+                </a>
+            </h1>
+
+            <div>
+                {{ $post->excerpt }}
+            </div>
+
+        </article>
+    @endforeach
+</body>
